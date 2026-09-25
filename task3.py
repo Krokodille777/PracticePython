959f60d70dff3dbbb6a53e8c9485c7a4b95f28cd
Тернавський Яромир Миколайович <krokocoder123@gmail.com>
2026-09-25 10:55:41 +0300

    Add initial implementation of Ginny's character dictionary and related operations

---
+++ task3.py
@@ -0,0 +1,48 @@
+ginnysDict = {
+    "fname": "Ginny",
+    "lname": "Gray",
+    "title": "The Marionette",
+    "age": 13,
+    "father": "Foster Gray (The Puppeteer)",
+    "mother": "Eleonora Gray (The Witch)",
+    "lives in":"The House of Grays",
+    "abilities": ["Healing", "Flying", "Baking", "Dancing", "Fight"],
+    "second_mother": "Lyra",
+    "hates": ["The Order", "Meanies", "spicy_food", "boredom"]
+}
+
+print(ginnysDict)
+
+#1 Add Item 
+
+new_item = {"best_friends": ["Toto", "Veronika"]}
+
+ginnysDict.update(new_item)
+
+
+#2 Nested Dictionary
+
+lovesDict = {
+    1: "Toto",
+    2: "Adventure",
+    3: "Her family",
+    4: "Knight Novels",
+    5: "Vikings"
+}
+
+ginnysDict["loves"] = lovesDict
+print(lovesDict[3])
+
+#3 Remove Item
+
+del ginnysDict["hates"]
+print(ginnysDict)
+
+#4 and 5 keys() and values()
+
+print(ginnysDict.keys())
+print(ginnysDict.values())
+
+#6 and 7 items() and update()
+print(ginnysDict.items())
+print(ginnysDict.update(("favorite_phrase", "You're all Big Meanies!")))
