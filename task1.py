0b8664bb112da2bf6802a937362b9ab9d2b2371c
Тернавський Яромир Миколайович <krokocoder123@gmail.com>
2026-09-25 10:10:34 +0300

    Add initial implementation of character list and related operations

---
+++ task1.py
@@ -0,0 +1,67 @@
+characters = ['Ginny', "The Puppeteer", "Lyra", "Mara", "Veronika", "Balthazar", "Toto" , "Elder Thorne"]
+print(characters)
+#1
+if "The Puppeteer" in characters:
+    print("The Puppeteer is in the list.")
+
+if "Toto" in characters:
+    print("Toto is in the list.")
+
+if "Ragnar" in characters:
+    print("Ragnar is in the list.")
+else:
+    print("Ragnar is not in the list.")
+
+
+#2
+
+selected_characters = characters[3: 6]
+print(selected_characters)
+
+#3 Add Gregor
+
+if "Gregor" not in characters:
+    characters.append("Gregor")
+    print (characters)
+
+#Add Eleonora
+
+if "Eleonora" not in characters:
+    characters.append("Eleonora")
+    print (characters)
+
+#4 Remove Balthazar
+
+if "Balthazar" in characters:
+    characters.pop(characters.index("Balthazar"))
+    print(characters)
+
+#5 Create a nest list and print one of it's elements
+
+more_characters= [characters, ["King Edward", "Princess Camille", "Lorenzo", "Rotter'Art", "Lord Arin", "Sabba'Art"]]
+
+print(more_characters[1][3])
+
+#6 Print every element from more_characters, using for and len
+
+for creature in range(len(characters)):
+    print(characters[creature])
+
+#7 Sort more_characters in alphabetic order and print it
+
+def sort_characters(characters_list):
+    for sublist in characters_list:
+        sublist.sort()
+    return characters_list
+
+characters = sort_characters(characters)
+print(characters)
+
+#8 Use count() method
+
+print(characters[0].count("Lyra"))
+
+#9 Use index() method
+
+print(characters[1].index("Toto"))
+
